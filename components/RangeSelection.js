ef2e2f0074870fda4df685dd6eda22c1e070e895
ablin <space6fic@gmail.com>
2021-07-02 01:07:42 +0200

    Added buttons to select a range and querying data

---
+++ components/RangeSelection.js
@@ -0,0 +1,51 @@
+import React, { useState } from "react";
+
+const RANGE_OPTIONS = [
+  "1H",
+  "2H",
+  "3H",
+  "4H",
+  "8H",
+  "12H",
+  "1D",
+  "3D",
+  "1W",
+  "1M",
+  "3M",
+  "6M",
+];
+
+export default function RangeSelection({ setData }) {
+  const [range, setRange] = useState("2H");
+
+  async function handleClick(range) {
+    try {
+      // TODO make button appearance change (pushed in)
+      const res = await fetch(`http://localhost:8080/api/scrape/${range}`);
+
+      if (res.status === 200) {
+        setRange(range);
+        const data = await res.json();
+        setData(data);
+      }
+    } catch (err) {
+      console.log("An error occured with the API");
+    }
+  }
+
+  setInterval(() => {
+    handleClick(range);
+  }, 1000 * 60 * 5);
+
+  return (
+    <div>
+      {RANGE_OPTIONS.map((range) => {
+        return (
+          <button key={range} onClick={() => handleClick(range)}>
+            {range}
+          </button>
+        );
+      })}
+    </div>
+  );
+}
---
+++ components/RangedData.js
@@ -0,0 +1,32 @@
+import React, { useState } from "react";
+import RangeSelection from "./RangeSelection";
+
+export default function RangedData({ data }) {
+  const [consumedData, setData] = useState(data);
+
+  return (
+    <>
+      <RangeSelection setData={setData} />
+
+      <div>
+        <h1>Average payout: {consumedData.avgPayout}</h1>
+        <h1>Average pool: {consumedData.avgPool}</h1>
+        <h1>Average diff: {consumedData.avgDiff}</h1>
+        <h1>Number of rounds: {consumedData.nbEntries}</h1>
+        <br />
+        <h1>Esperance when betting 10 $: {consumedData.safeEsperance} $</h1>
+        <h1>
+          Safe average payout: {consumedData.avgSafe} -{" "}
+          {consumedData.safePercentWr}%
+        </h1>
+        <h1>
+          Risky average payout: {consumedData.avgRisky} -{" "}
+          {consumedData.riskyPercentWr}%
+        </h1>
+        <br />
+        <h2>Number of down round: {consumedData.nbRoundDOWN}</h2>
+        <h2>Number of up round: {consumedData.nbRoundUP}</h2>
+      </div>
+    </>
+  );
+}
