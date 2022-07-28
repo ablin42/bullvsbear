452a9ac444a2dd662ae7dbcba170fd573e0e3e06
ablin <space6fic@gmail.com>
2022-07-28 22:06:39 +0200

    updated API_HOST

--- api_host.js
+++ api_host.js
@@ -1,4 +1,4 @@
-const API_HOST = "https://pcs-predictions.herokuapp.com";
+const API_HOST = 'https://pcs-prediction-api.herokuapp.com';
 // https://pcs-predictions.herokuapp.com
 // http://localhost:8080
 module.exports = {
